e2cb0999183e349838993e7c922d7e6e9972d225
Victor Sandoval <victor.sandoval@vallegrande.edu.pe>
2022-02-15 22:03:38 -0500

    Update Document in Model

--- src/main/java/com/nttdata/customerservice/model/Customer.java
+++ src/main/java/com/nttdata/customerservice/model/Customer.java
@@ -3,10 +3,12 @@ package com.nttdata.customerservice.model;
 
 import lombok.Data;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDate;
 
 @Data
+@Document
 public class Customer {
     @Id
     private String id;
